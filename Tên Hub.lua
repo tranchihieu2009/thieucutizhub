bd758095cd4838828f89dda7973df491945ab92b
tranchihieu2009 <158511861+tranchihieu2009@users.noreply.github.com>
2024-02-02 12:45:22 +0700

    Update Tên Hub.lua

--- Tên Hub.lua
+++ Tên Hub.lua
@@ -3,7 +3,7 @@ local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/d
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
 --------------------------------------------------------------------------------------------------------------------------------------------
 local Window = Fluent:CreateWindow({
-    Title = "Cheems Hub",
+    Title = "CheemsHub",
     SubTitle = "by Trần Hiếu",
     TabWidth = 160,
     Size = UDim2.fromOffset(450, 300),
